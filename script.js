629f872c5ba6e5bc81524867ac951d7f6f0529de
rishimalviya <malviyaabhi059@gmail.com>
2025-02-02 21:37:44 +0530

    100ques

--- script.js
+++ script.js
@@ -96,12 +96,14 @@
 // const validEmail = emails.filter(email => email.includes('@'));
 // console.log(validEmail);
 
-const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-const isPrime = num => {
-    if (num <= 1 ) return false
-for (let i =2 ; i<= Math.sqrt(num);i++){
-    if(num % i === 0) return false}
-return true;
-}
-const primeNumbers = numbers.filter(isPrime);
+// const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const isPrime = num => {
+//     if (num <= 1 ) return false
+// for (let i =2 ; i<= Math.sqrt(num);i++){
+//     if(num % i === 0) return false}
+// return true;
+// }
+// const primeNumbers = numbers.filter(isPrime);
+
+// 100 ques =----------------
 
